97804e71f579378d752ef645ff6a2c300ac6443e
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 11:30:04 -0500

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,25 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Cube
+{
+	private int side;
+	private int surfaceArea;
+
+	public void setSide(int s)
+	{
+	}
+
+	public void calculateSurfaceArea( )
+	{
+	}
+
+	public void print( )
+	{
+	}
+}
---
+++ CubeRunner.java
@@ -0,0 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class CubeRunner
+{
+	public static void main( String[] args )
+   {
+		Cube test = new Cube();
+	 	test.setSide(112);
+	 	test.calculateSurfaceArea();
+	 	test.print();
+
+	 	//add more test cases
+	 	
+	 	
+	 	
+	 	
+	}
+}
